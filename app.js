cdca6cf69ee3e9dae58ed8e7129c219957349a41
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-06-20 15:42:50 -0400

    SBA DONE

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import { getCatimages } from "./CatData.js";
+
 const button = document.querySelector("button")
 button.addEventListener("click",async ()=>{
+    console.log(getCatimages)
     await getCatimages()
 })
